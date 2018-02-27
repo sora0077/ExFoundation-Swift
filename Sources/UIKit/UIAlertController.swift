c17126eb9ce9029fb7c61c95496ed01c162fe3cd
sora0077 <t.hayashi0077+github@gmail.com>
2018-02-27 21:30:11 +0900

    Add UIAlertController helper

---
+++ Sources/UIKit/UIAlertController.swift
@@ -0,0 +1,15 @@
+//
+//  UIAlertController.swift
+//  FoundationSupport
+//
+//  Created by 林達也 on 2018/02/27.
+//  Copyright © 2018年 林達也. All rights reserved.
+//
+
+import UIKit
+
+extension UIAlertController {
+    public func addAction(title: String?, style: UIAlertActionStyle = .default, handler: ((UIAlertAction) -> Void)? = nil) {
+        addAction(UIAlertAction(title: title, style: style, handler: handler))
+    }
+}
